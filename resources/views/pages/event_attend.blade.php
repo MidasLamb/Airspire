f0ac75d6907024ea074a14a3b8ad6c9c02bd074f
MidasLamb <midas_lambrichts@hotmail.com>
2016-03-05 12:10:15 +0100

    Bugfix 2

--- resources/views/pages/event_attend.blade.php
+++ resources/views/pages/event_attend.blade.php
@@ -10,9 +10,11 @@
 {{--*/ $title = 'Event attendence' /*--}}
 
 @section('content')
+<script>
 FB.ui({
   method: 'feed',
   link: 'https://developers.facebook.com/docs/',
   caption: 'An example caption',
 }, function(response){});
+</script>
 @stop
