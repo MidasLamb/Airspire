4034c98974bdf642f46ae7566dc7841ffa7b8a09
MidasLamb <midas_lambrichts@hotmail.com>
2016-03-29 14:34:47 +0200

    Restarted auth

--- resources/views/pages/events/events.blade.php
+++ resources/views/pages/events/events.blade.php
@@ -14,15 +14,24 @@
     <h1>Events</h1>
 
     @if (count($events) > 0)
-    <ul>
-    @foreach($events as $event)
-      <a href="{{URL::route('event',array('id'=>$event->id))}}">
-        <div class="thumbnail">
-          <li><h3>{{ $event->title }}</h3>{{ $event->description }}</li>
-        </div>
-      </a>
-    @endforeach
-    </ul>
+      <ul>
+      @foreach($events as $event)
+        <a href="{{URL::route('event',array('id'=>$event->id))}}">
+          <div class="thumbnail">
+              <li>
+                <div class= "media">
+                  <img class="media-object pull-right" alt="Flag of {{$event->country_name}}" src="/flags/{{$event->country_flag}}" style="max-width:30%; padding-bottom: 15px;">
+                  <div class= "media-body">
+                    <h3>{{ $event->title }}</h3>
+                    {{ $event->excerpt }}
+                  </div>
+                </div>
+
+              </li>
+          </div>
+        </a>
+      @endforeach
+      </ul>
     @else
         No events planned.
     @endif
