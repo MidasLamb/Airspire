ca6af0a019be495c001c72bcfded114ac326cf08
MidasLamb <midas_lambrichts@hotmail.com>
2016-04-07 17:20:35 +0200

    Added ;

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -66,7 +66,7 @@ class PagesController extends Controller
 
         $data['passed_events'] = Event::where('ended_at', '<', date('Y-m-d   H:i:s'))->get();
 
-        $data['possible_attended_events'] = array_merge($data['active_events'], $data['passed_events'])
+        $data['possible_attended_events'] = array_merge($data['active_events'], $data['passed_events']);
 
 
         if ($data['loggedin']){
